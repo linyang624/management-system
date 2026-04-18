7a0a5ae651da88bc14acbfacfa3c273e8a305bdb
jz450 <jieying.zhang@duke.edu>
2026-04-18 00:40:48 -0700

    hash passoword function

--- management-system--/backend/models/User.js
+++ management-system--/backend/models/User.js
@@ -1,24 +1,48 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+//design user schema
 const userSchema = new mongoose.Schema(
-    {
-        email: {
-           type: String,
-           required: true, 
-           unique: true,
-        },
-        password: {
-           type: String,
-           required: true, 
-        },
-        role: {
-           type: String,
-           required: true, 
-           enum: ['admin', 'user'],
-           default: 'user',
-        },
-    }    
+   {
+      email: {
+         type: String,
+         required: true, 
+         unique: true,
+      },
+      password: {
+         type: String,
+         required: true, 
+      },
+      role: {
+         type: String,
+         required: true, 
+         enum: ['admin', 'user'],
+         default: 'user',
+      },
+   },
+   {
+      timestamps: true,
+   }   
 );
+
+//hash password before saving to database
+try {
+   userSchema.pre('save', async function (next) {
+      if (!this.isModified('password')) {
+         return next();
+      }
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+   })
+}
+catch(error) {
+   next(error);
+}
+
+//comapare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+   return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema)
 
