e40e5e98c1591ff8ef4f2ac581f1c5e548cb6a26
jz450 <jieying.zhang@duke.edu>
2026-04-19 00:51:37 -0700

    build authApi.js

---
+++ management-system--/src/api/authApi.js
@@ -0,0 +1,57 @@
+const BASE_URL = 'http://localhost:5001/api/auth';
+
+async function handleResponse(response) {
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || 'Request failed');
+    }
+    return data;
+}
+
+//signin
+export async function signInApi(formData) {
+    const response = await fetch(`${BASE_URL}/signin`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    });
+    return handleResponse(response);
+}
+
+//signup
+export async function signUpApi(formData) {
+    const response = await fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    });
+    return handleResponse(response);
+}
+
+//update-password
+export async function updatePasswordApi(formData) {
+    const response = await fetch(`${BASE_URL}/update-password`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    });
+    return handleResponse(response);
+}
+
+//logout
+export async function logOutApi(formData) {
+    const response = await fetch(`${BASE_URL}/logout`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    return handleResponse(response);
+}
