80ac54b48a23cdad45de7d1e7feed238decb7d96
jz450 <jieying.zhang@duke.edu>
2026-04-21 16:13:36 -0700

    debug

--- management-system--/backend/server.js
+++ management-system--/backend/server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import connectDB from '../config/db.js';
-import authRoutes from '../routes/authRoutes.js';
-import errorHandler from '../middlewares/error.js';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import errorHandler from './middlewares/error.js';
 
 dotenv.config();
 
--- management-system--/backend/utils/error.js
+++ management-system--/backend/utils/error.js
@@ -1,3 +1,17 @@
 export default function errorHandler(err, req, res, next) {
   return res.status(err.statusCode ?? 500).json({ message: err.message });
 }
+
+export class ValidationError extends Error {
+
+  constructor(message) {
+
+    super(message);
+
+    this.name = 'ValidationError';
+
+    this.statusCode = 400;
+
+  }
+
+}
