209ed5f74dccfbe88182d866ef5bfbbe11f3bd9c
jz450 <jieying.zhang@duke.edu>
2026-04-22 18:47:20 -0700

    add validator to logout

--- management-system--/backend/routes/productRoutes.js
+++ management-system--/backend/routes/productRoutes.js
@@ -1,1 +1,15 @@
+import express from 'express';
+import { createProduct, getProducts, getProductById, updateProduct, deleteProduct } from '../controller/productController.js';
+import { validate } from '../middlewares/validation.js';
 import { verifyToken, checkAdmin } from "../middlewares/auth.js";
+import { createProductValidation, updateProductValidation } from '../middlewares/productValidators.js';
+const router = express.Router();
+
+//auth routes
+router.post('/signin', signInValidation, validate, signIn);
+router.post('/signup', signUpValidation, validate,signUp);
+router.put('/update-password', updatePasswordValidation, validate, updatePassword);
+router.post('/logout', logOut);
+
+//module.exports = router;
+export default router;
